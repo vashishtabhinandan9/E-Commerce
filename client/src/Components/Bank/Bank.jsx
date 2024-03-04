909fd9216d0c0bb8cb557fd4cbe244caf3a07cca
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2024-03-05 00:03:39 +0530

    Bank Content Panel

--- client/src/Components/Bank/Bank.jsx
+++ client/src/Components/Bank/Bank.jsx
@@ -1,16 +1,76 @@
 import { Dialog, Transition } from '@headlessui/react'
-import { Fragment, useState } from 'react'
+import { PiCoinsBold } from "react-icons/pi";
+import { BiSolidDiscount } from "react-icons/bi";
+import { FaShareAlt } from "react-icons/fa";
 
 export default function Bank() {
  
+  const Content=[
+    {
+      Heading:"Coins",
+      Description:`Use Coins to Buy Prdoucts. 🤑`,
+      Extra:<span className=' my-2 mx-auto text-3xl flex justify-center gap-5'>
+      <PiCoinsBold className='text-5xl  text-orange-300'/> 0
+      </span> 
+    },
+    {
+      Heading:"Coupon",
+      Description:`Use Coupon to Get Heavy Discounts. 🤩`,
+      Extra:<span className='my-2 mx-auto text-3xl flex justify-center align-center gap-5'>
+      <BiSolidDiscount className='text-5xl  text-red-500'/> 
+      <div>0</div>
+      </span>  
+    },
+    {
+      Heading:"Referral",
+      Description:`Give your friends a reward and claim your own when they make a purchase. 💸`,
+      Extra:<div className='my-3 mx-auto text-3xl flex-col  justify-center align-center gap-5'>
+      
+      <span className='flex  justify-center  align-center gap-2'>
+      <FaShareAlt className='text-4xl   text-green-500'/> 
+      <p className='text-xl font-semibold '>"They get <br/>10% off coupon"</p>
+      </span>
+
+      <span className='flex  justify-center align-center gap-2'>
+      <FaShareAlt className='text-4xl   text-green-500'/> 
+      <p className='text-xl font-semibold '>"You get <br/>10% off coupon"</p>
+      </span>
+
+      </div> 
+       
+    }
+  ]
+  
   return (
     <>
- 
-  <div>
-    <div className='h-24 bg-gradient-to-t from-40% from-[#001134] to-[#02256C] text-center text-white'   >
-      Welcome to Bank
-    </div>
-  </div>
+      <div className="w-full ">
+        {
+          Content.map((item,index)=>{
+            
+            return(
+              
+              <div
+              
+              key={index}
+              id="Bank_Coins"
+              className=" h-max w-5/6 rounded-lg text-center text-black  text-2xl font-bold bg-white shadow-xl my-4 mx-auto border-red-800 border-2"
+            >
+              <h1 className='antialiased'>{item.Heading}</h1>
+              {item.Description}
+
+              {item.Extra}
+             
+            </div> 
+            )
+          })
+        }
+      
+
+        <div>
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
+
--- client/src/Components/Slide_Menu/Up_Slide_Menu.jsx
+++ client/src/Components/Slide_Menu/Up_Slide_Menu.jsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
-import { RxCross2 } from "react-icons/rx";
 import { Dialog, Transition } from '@headlessui/react'
 import { SignIn } from "../Auth/Auth";
 import Bank from "../Bank/Bank";
+import { RxCross2 } from "react-icons/rx";
+import { useRef } from 'react';
+import { motion } from "framer-motion"
+
 export function Up_Slide_Menu({Icon, Name}) {
+
   const [Isopen, setIsopen] = useState(false);
-  
-   const Handle_SideMenu=()=> {
-    //EventTarget.id === "Hamburger_Menu" ? setDir(false) : setDir(true);
-    setIsopen(true);
-    // Update the condition to check the value of the Name prop
-    
-  }
+//   const scrollableDivRef = useRef(null);
+//   const [is, setScrollPosition] = useState(10);
+//   let scrollPosition;
+//   let sc;
+// let percentagescroll;
+//   //const scrollableDiv = document.getElementById("Heading_Before_scroll");
+
+//   const handleScroll = () => {
+//      scrollPosition = parseInt(scrollableDivRef.current.scrollTop);
+//      percentagescroll= scrollPosition*10;
+//      sc=100/percentagescroll;
+     
+//     // console.log(parseInt(percentagescroll*10));
+//    // scrollPosition > 100 ?  setScrollPosition(true): setScrollPosition(false);
+//     // const scrollValue = scrollableDiv.scrollTop;
+//     // scrollableDiv.style.height = `${scrollPosition}px`;
+//     setScrollPosition(Math.min(parseInt(percentagescroll),20));
+//     console.log(sc);
+//   };
+
+
+  console.log("rerender");
 
   return (
     <>
@@ -25,30 +44,45 @@ export function Up_Slide_Menu({Icon, Name}) {
         {Icon}
       </button>
 
-      {Isopen && ( //for the backdrop on large screen
-        <div
-          className="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-20"
-          onClick={() => setIsopen(false)}
-        ></div>
-      )}
+     
       <aside
         id="Side_Menu"
-        className={`fixed top-0 right-0 w-screen md:w-1/3 overflow-auto  border-black border-2 h-screen z-30  bg-white transform transition-all ease-out duration-500
-        ${Isopen ? "translate-y-0" : "translate-y-full"} `}
+        // onScroll={handleScroll}
+        className={`fixed top-0 right-0 w-screen h-screen md:w-1/3 overflow-auto  border-black border-2  z-10  bg-slate-100  transform transition-all ease-out duration-500
+        ${Isopen ? "translate-y-0" : "translate-y-full"}  `}
       >
-        <div>
-          <div id="close" className=" w-8 h-8 m-4 float-right text-white  ">
-            <button
-              className="w-full h-full"
-              onClick={() => {
-                setIsopen(false);
-              }}
-            >
-              <RxCross2 className="w-full h-full " />
-            </button>
+         <div
+          id="close"
+          className=" sticky top-2 float-right z-10 w-8 h-8 m-2  text-white  "
+        >
+          <button
+            className="w-full h-full"
+            onClick={() => {
+              setIsopen(false);
+            }}
+          >
+            <RxCross2 className="w-full h-full " />
+          </button>
           </div>
-          <div>{Name == "Bank_Menu" ? <Bank /> : <Bank />}</div>
+
+          
+          
+        <div
+          id="Heading_Before_scroll"
+          
+          className={`   sticky top-0 w-full h-80  rounded-b-3xl border-red-800 border-2 bg-gradient-to-t from-40% from-[#001134] to-[#02256C] text-center text-white`}>
+         
+        
+            <h3
+          >
+              Welcome to <br />
+              Your Money Bank
+            </h3>
+         
+          
         </div>
+
+        {Name == "Bank_Menu" ? <Bank /> : <Bank />}
       </aside>
     </>
   );
