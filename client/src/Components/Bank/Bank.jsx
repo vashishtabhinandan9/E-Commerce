9a22f20291b0ba8c4ce0a92c42ac6aa88cf37d43
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2024-03-06 09:29:01 +0530

    Bank content panel 2

--- client/src/Components/Bank/Bank.jsx
+++ client/src/Components/Bank/Bank.jsx
@@ -4,37 +4,41 @@ import { FaShareAlt } from "react-icons/fa";
 
 import { Popover, Transition } from '@headlessui/react'
 import { IoChevronDownSharp } from "react-icons/io5";
+import { GoGift } from "react-icons/go";
+import { FiShoppingBag } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { LuUserPlus } from "react-icons/lu";
 import { Fragment } from 'react'
 
-function Example() {
+function BankPopUp() {
   
 const solutions = [
   {
     name: 'SingUp',
     description: 'Earn 300 Coins',
-    icon: <IoChevronDownSharp/>,
+    icon: <LuUserPlus/>,
   },
   {
     name: 'Place an Order',
     description: 'Earn 300 Coins',
     
-    icon: <IoChevronDownSharp/>,
+    icon: <FiShoppingBag/>,
   },
   {
     name: 'Write a Product Review',
     description:'Earn 100 Coins',
     
-    icon: <IoChevronDownSharp/>,
+    icon: <GoGift/>,
   },
   {
     name: 'Follow On Instagram',
     description: 'Earn 100 Coins',
-    icon: <IoChevronDownSharp/>,
+    icon: <FiInstagram/>,
   },
   {
     name: 'Share with Friends',
     description: 'Earn 200 Coins',
-    icon: <IoChevronDownSharp/>,
+    icon: <FaShareAlt/>,
   },
 ]
 
@@ -74,17 +78,17 @@ const solutions = [
                       <>
                       
                       <div
-                        key={index}
-                        className="-m-3 flex justify-around items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
+                        key={item.name}
+                        className="-m-3 flex justify-center gap-4 items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
                       >
                        <div >
                         {item.icon}
                         </div> 
-                        <div className="w-3/5">
-                          <p className="text-lg font-semibold text-gray-900">
+                        <div className="w-2/3">
+                          <p className="text-lg font-medium text-gray-900">
                             {item.name}
                           </p>
-                          <p className="text-sm font-meduim text-gray-500">
+                          <p className="text-sm font-normal text-nowrap text-gray-500">
                             {item.description}
                           </p>
                         </div>
@@ -117,13 +121,16 @@ export default function Bank() {
       Heading:"Coins",
       Description:`Use Coins to Buy Prdoucts. 🤑`,
       Extra:
-      <div className=' my-2 mx-auto text-3xl flex justify-center gap-5'>
-        <span >
+      <>
+       <div className=' my-2 mx-auto text-2xl flex gap-5'>
+        <span className="flex" >
       <PiCoinsBold className='text-5xl  text-orange-300'/> 0
       </span> 
-      SeeMore ways to earn
-      <Example/>
+      
       </div>
+      <span>SeeMore ways to earn <BankPopUp/></span>
+      </>
+     
       
     },
     {
@@ -137,16 +144,16 @@ export default function Bank() {
     {
       Heading:"Referral",
       Description:`Give your friends a reward and claim your own when they make a purchase. 💸`,
-      Extra:<div className='my-3 mx-auto text-3xl flex-col  justify-center align-center gap-5'>
+      Extra:<div className='my-3 mx-auto text-3xl flex-col  gap-4 align-center'>
       
-      <span className='flex  justify-center  align-center gap-2'>
+      <span className='flex  justify-center  align-center gap-4 '>
       <FaShareAlt className='text-4xl   text-green-500'/> 
-      <p className='text-xl font-semibold '>"They get <br/>10% off coupon"</p>
+      <p className='text-xl font-semibold text-left'>They get <br/>10% off coupon</p>
       </span>
 
       <span className='flex  justify-center align-center gap-2'>
       <FaShareAlt className='text-4xl   text-green-500'/> 
-      <p className='text-xl font-semibold '>"You get <br/>10% off coupon"</p>
+      <p className='text-xl font-semibold text-left '>You get <br/>10% off coupon</p>
       </span>
 
       </div> 
@@ -165,11 +172,11 @@ export default function Bank() {
               <div
               
               key={index}
-              id="Bank_Coins"
-              className=" h-max w-5/6 rounded-lg text-center text-black  text-2xl font-bold bg-white shadow-xl my-4 mx-auto border-red-800 border-2"
+              id="Bank_Content_Repeater"
+              className=" h-max w-5/6 rounded-lg text-center bg-white shadow-xl my-4 mx-auto border-red-800 border-2"
             >
-              <h1 className='antialiased'>{item.Heading}</h1>
-              {item.Description}
+              <h1 className='antialiased text-xl font-medium text-black'>{item.Heading}</h1>
+              <p className="font-normal px-4 text-slate-600">{item.Description}</p>
 
               {item.Extra}
              
