3ab93f7f9a1615fa31e87f42dcb2c41ec1169e23
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2024-05-12 21:09:23 +0530

    product page started

---
+++ client/src/Components/Products/Single_Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Single_product({Product_Data}) {
+  return (
+    <>
+
+      <a key={Product_Data.id} href={Product_Data.href} className="group ">
+        <div className="border-2 border-black">
+          <img
+            src={Product_Data.imageSrc}
+            alt={Product_Data.imageAlt}
+            className="h-full w-full object-cover object-center group-hover:opacity-75"
+          />
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">{Product_Data.name}</h3>
+        <p className="my-4 text-lg font-medium text-gray-900">
+          {Product_Data.price}
+        </p>
+      </a>
+    </>
+  );
+}
--- client/src/Pages/Collection.jsx
+++ client/src/Pages/Collection.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Filter_Sort_Menu from '../Components/Filter&Sort/Filter&Sort'
+import Single_product from '../Components/Products/Single_product'
+
 export default function Collection() {
   const links = [
     { href: '/account-settings', label: 'Account settings' },
@@ -7,10 +9,58 @@ export default function Collection() {
     { href: '/license', label: 'License' },
     { href: '/sign-out', label: 'Sign out' },
   ]
+  //get global collection data filter it based on the route path params you get so it here 
+  const Products = [
+    {
+      id: 1,
+      name: 'Earthen Bottle',
+      href: '#',
+      price: '$48',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+      id: 2,
+      name: 'Nomad Tumbler',
+      href: '#',
+      price: '$35',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 3,
+      name: 'Focus Paper Refill',
+      href: '#',
+      price: '$89',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 4,
+      name: 'Machined Mechanical Pencil',
+      href: '#',
+      price: '$35',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    // More products...
+  ]
+
   return (
-  <>
-  <Filter_Sort_Menu/>
-  <div>Collection</div>
-  </>
-  )
+    <>
+      <Filter_Sort_Menu />
+      <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        {Products.map((item, index) => {
+          return (
+            <div key={index}>
+              <Single_product Product_Data={item} />
+            </div>
+          );
+        })}
+      </div>
+      </div>
+   
+    </>
+  );
 }
