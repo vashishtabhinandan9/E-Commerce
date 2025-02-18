157ccc5a7fccaea1eb03f4cb2a7ec7cb82ca8aa5
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2025-02-18 17:53:12 +0530

    backend deployment 3

--- server/src/ZodSchema/AuthSchema.js
+++ server/src/ZodSchema/AuthSchema.js
@@ -1,16 +1,16 @@
-import zod from 'Zod'
+import { z } from 'zod'; // Correct import statement
 
-const SignInSchema = zod.object({
-    Email: zod.string().email('Invalid email address'),
-    Password: zod.string().min(6, 'Password should be at least 6 characters long'),
+const SignInSchema = z.object({
+    Email: z.string().email('Invalid email address'),
+    Password: z.string().min(6, 'Password should be at least 6 characters long'),
 });
 
-const SignUpSchema = zod.object({
-    FirstName: zod.string().min(1, 'First Name is required'),
-    LastName: zod.string().min(1, 'Last Name is required'),
-    Phone: zod.string().length(10, 'invalid Phone NUmber'),
-    Email: zod.string().email('Invalid email address'),
-    Password: zod.string().min(6, 'Password should be at least 6 characters long'),
+const SignUpSchema = z.object({
+    FirstName: z.string().min(1, 'First Name is required'),
+    LastName: z.string().min(1, 'Last Name is required'),
+    Phone: z.string().length(10, 'invalid Phone NUmber'),
+    Email: z.string().email('Invalid email address'),
+    Password: z.string().min(6, 'Password should be at least 6 characters long'),
 });
 
-export { SignInSchema, SignUpSchema }
+export { SignInSchema, SignUpSchema };
